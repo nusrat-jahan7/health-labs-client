d02b4440bdf5b4d506001356e7e68b92991046d6
nusrat-jahan7 <nusrat.jahan03060@gmail.com>
2023-11-30 14:58:12 +0600

    reviews added

--- src/pages/Reviews.jsx
+++ src/pages/Reviews.jsx
@@ -1,4 +1,8 @@
-import React from "react";
+import { FaQuoteLeft, FaQuoteRight, FaStar } from "react-icons/fa6";
+import client1 from "/images/client-1.jpg";
+import client2 from "/images/client-2.jpg";
+import client3 from "/images/client-3.jpg";
+import client4 from "/images/client-4.jpg";
 
 const Reviews = () => {
   return (
@@ -9,6 +13,112 @@ const Reviews = () => {
             <h1 className="text-7xl font-bold text-blue-600 uppercase">
               What Our Client Say!
             </h1>
+          </div>
+        </div>
+      </div>
+
+      <div className=" mx-auto px-5 grid grid-cols-1 lg:grid-cols-2 ">
+        <div className="lg:flex items-center  bg-lime-100 w-9/12 mx-auto shadow-xl mt-8 rounded-t-full gap-6 lg:rounded-l-full rounded-r-xl">
+          <figure className="w-72 flex justify-center items-center mx-auto">
+            <img src={client1} alt="Shoes" className="w-72 h-72 rounded-full" />
+          </figure>
+          <div className="flex-1 pt-8 pr-8 pl-8 pb-4">
+            <div className="flex gap-2 text-3xl text-yellow-400 my-4">
+              <FaStar></FaStar>
+              <FaStar></FaStar>
+              <FaStar></FaStar>
+              <FaStar></FaStar>
+              <FaStar></FaStar>
+            </div>
+            <h2 className="card-title">
+              <FaQuoteLeft className="text-3xl my-3 text-gray-400" />
+            </h2>
+            <p>
+              Very good Service!I recommend HealthLab Diagnostic Center. I got
+              my test result very fast. Very useful diagnostic center for online
+              as well as offline for booking appointment at any time.
+            </p>
+            <h2 className="card-title flex justify-end pr-5">
+              <FaQuoteRight className="text-3xl text-right my-3 text-gray-400" />
+            </h2>
+          </div>
+        </div>
+
+        <div className="lg:flex items-center  bg-lime-100 w-9/12 mx-auto shadow-xl mt-8 rounded-t-full gap-6 lg:rounded-l-full rounded-r-xl">
+          <figure className="w-72 flex justify-center items-center mx-auto">
+            <img src={client2} alt="Shoes" className="w-72 h-72 rounded-full" />
+          </figure>
+          <div className="flex-1 pt-8 pr-8 pl-8 pb-4">
+            <div className="flex gap-2 text-3xl text-yellow-400 my-4">
+              <FaStar></FaStar>
+              <FaStar></FaStar>
+              <FaStar></FaStar>
+              <FaStar></FaStar>
+              <FaStar></FaStar>
+            </div>
+            <h2 className="card-title">
+              <FaQuoteLeft className="text-3xl my-3 text-gray-400" />
+            </h2>
+            <p>
+              Very good Service!I recommend HealthLab Diagnostic Center. I got
+              my test result very fast. Very useful diagnostic center for online
+              as well as offline for booking appointment at any time.
+            </p>
+            <h2 className="card-title flex justify-end pr-5">
+              <FaQuoteRight className="text-3xl text-right my-3 text-gray-400" />
+            </h2>
+          </div>
+        </div>
+
+        <div className="lg:flex items-center  bg-lime-100 w-9/12 mx-auto shadow-xl mt-8 rounded-t-full gap-6 lg:rounded-l-full rounded-r-xl">
+          <figure className="w-72 flex justify-center items-center mx-auto">
+            <img src={client3} alt="Shoes" className="w-72 h-72 rounded-full" />
+          </figure>
+          <div className="flex-1 pt-8 pr-8 pl-8 pb-4">
+            <div className="flex gap-2 text-3xl text-yellow-400 my-4">
+              <FaStar></FaStar>
+              <FaStar></FaStar>
+              <FaStar></FaStar>
+              <FaStar></FaStar>
+              <FaStar></FaStar>
+            </div>
+            <h2 className="card-title">
+              <FaQuoteLeft className="text-3xl my-3 text-gray-400" />
+            </h2>
+            <p>
+              Very good Service!I recommend HealthLab Diagnostic Center. I got
+              my test result very fast. Very useful diagnostic center for online
+              as well as offline for booking appointment at any time.
+            </p>
+            <h2 className="card-title flex justify-end pr-5">
+              <FaQuoteRight className="text-3xl text-right my-3 text-gray-400" />
+            </h2>
+          </div>
+        </div>
+
+        <div className="lg:flex items-center  bg-lime-100 w-9/12 mx-auto shadow-xl mt-8 rounded-t-full gap-6 lg:rounded-l-full rounded-r-xl">
+          <figure className="w-72 flex justify-center items-center mx-auto">
+            <img src={client4} alt="Shoes" className="w-72 h-72 rounded-full" />
+          </figure>
+          <div className="flex-1 pt-8 pr-8 pl-8 pb-4">
+            <div className="flex gap-2 text-3xl text-yellow-400 my-4">
+              <FaStar></FaStar>
+              <FaStar></FaStar>
+              <FaStar></FaStar>
+              <FaStar></FaStar>
+              <FaStar></FaStar>
+            </div>
+            <h2 className="card-title">
+              <FaQuoteLeft className="text-3xl my-3 text-gray-400" />
+            </h2>
+            <p>
+              Very good Service!I recommend HealthLab Diagnostic Center. I got
+              my test result very fast. Very useful diagnostic center for online
+              as well as offline for booking appointment at any time.
+            </p>
+            <h2 className="card-title flex justify-end pr-5">
+              <FaQuoteRight className="text-3xl text-right my-3 text-gray-400" />
+            </h2>
           </div>
         </div>
       </div>
